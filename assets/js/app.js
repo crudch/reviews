8a0d958b015549aa4c909659004495a23771a37e
Skip <skiphog@gmail.com>
2020-12-06 22:54:34 +0300

    add normalize

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
 /*window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';*/
 
-//window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery');
 
 //require('bootstrap');
 
 
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+//import UIkit from 'uikit';
+//import Icons from 'uikit/dist/js/uikit-icons';
 
 // loads the Icon plugin
-UIkit.use(Icons);
+//UIkit.use(Icons);
 
-window.UIkit = UIkit;
+//window.UIkit = UIkit;
 
